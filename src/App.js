140c4a56bca7479a4c914773cce7a67075c1c719
Marat Gaipov <marattig@gmail.com>
2020-10-28 19:51:45 -0700

    table, reactstrap added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
+import Customers from './Customers';
 import './App.css';
+
+const customers = [
+  {
+    id: 1, name: "Seytech", lastName: "Co", avatar: "https://www.seytech.co/images/logo.png",
+    email: "support@seytech.co", state: "WA", phone: 1234567703,
+    role: "student", github: "seytechschool", courses: ["js, react, algo"],
+    payment: 12000
+  },
+  {
+    id: 2, name: "Eliza", lastName: "Co", avatar: "https://avatars1.githubusercontent.com/u/68719361?s=100&v=4",
+    email: "support@seytech.co", state: "WA", phone: 1234567703,
+    role: "student", github: "seytechschool", courses: ["js, react, algo"],
+    payment: 12000
+  },
+  {
+    id: 3, name: "Adilet", lastName: "Co", avatar: "https://avatars0.githubusercontent.com/u/55602501?s=100&v=4",
+    email: "support@seytech.co", state: "WA", phone: 1234567703,
+    role: "instructor", github: "seytechschool", courses: ["js, react, algo"],
+    payment: 12000
+  },
+  {
+    id: 4, name: "Max", lastName: "Co", avatar: "https://avatars0.githubusercontent.com/u/40704457?s=100&v=4",
+    email: "support@seytech.co", state: "WA", phone: 1234567703,
+    role: "student", github: "seytechschool", courses: ["js, react, algo"],
+    payment: 12000
+  },
+  {
+    id: 5, name: "Ulan", lastName: "Co", avatar: "https://avatars1.githubusercontent.com/u/16879917?s=100&v=4",
+    email: "support@seytech.co", state: "WA", phone: 1234567703,
+    role: "admin", github: "seytechschool", courses: ["js, react, algo"],
+    payment: 12000
+  },
+]
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      value: 0,
+      customers,
     }
   }
 
@@ -14,8 +48,9 @@ class App extends Component {
 
   render(){
     return (
-      <div>
-        <h1> DAta</h1>
+      <div className="container">
+        <h1> Seytech Customers</h1>
+        <Customers customers={this.state.customers} />
       </div>
     )
   }
---
+++ src/Customers.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import { Table } from 'reactstrap';
+import './App.css';
+
+class Customers extends Component {
+
+  render(){
+    const {customers} = this.props;
+    return (
+      <div>
+        <Table striped className="customers">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Avatar</th>
+              <th>Full Name</th>
+              <th>State</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Payment</th>
+              <th>Courses</th>
+              <th>Role</th>
+              <th>Github</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              customers.map(customer=>{
+                const { id, name, lastName, avatar, email, state, phone,
+                role, github, courses, payment, status } = customer;
+                return (
+                <tr>
+                  <th scope="row">{id}</th>
+                  <td><img src={avatar} /></td>
+                  <td> <a href="/customer/1">{name} {lastName}</a> </td>
+                  <td>{state}</td>
+                  <td>{email}</td>
+                  <td>{phone}</td>
+                  <td>{payment}</td>
+                  <td>{courses}</td>
+                  <td>{role}</td>
+                  <td>{github}</td>
+                  <td>Actions</td>
+                </tr>
+                )
+              })
+            }
+            
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default Customers;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const data = [
+  {countryName: "USA", currency:"dollar", products:[
+    {name: "apple", active:true, subProducts:[
+      {name: "iPhone", price: 40, sold:4},
+      {name: "iPad", price: 530, sold:4},
+      {name: "watch", price: 530},
+    ]}
+  ]},
+  {countryName: "Russia", currency:"rubl", products:[
+    {name: "apple", active:true, subProducts:[
+      {name: "iPhone", price: 40, sold:4},
+      {name: "iPad", price: 530, sold:4},
+      {name: "watch", price: 530},
+    ]}
+  ]}
+]
+*/

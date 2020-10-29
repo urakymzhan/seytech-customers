7090b0fa7d38e328516e172f0ea6274602283b76
Marat Gaipov <marattig@gmail.com>
2020-10-28 19:04:22 -0700

    updated readme file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      value: 0,
+    }
+  }
+
+  
+
+  render(){
+    return (
+      <div>
+        <h1> DAta</h1>
+      </div>
+    )
+  }
 }
 
+
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const data = [
+  {countryName: "USA", currency:"dollar", products:[
+    {name: "apple", active:true, subProducts:[
+      {name: "iPhone", price: 40, sold:4},
+      {name: "iPad", price: 530, sold:4},
+      {name: "watch", price: 530},
+    ]}
+  ]},
+  {countryName: "Russia", currency:"rubl", products:[
+    {name: "apple", active:true, subProducts:[
+      {name: "iPhone", price: 40, sold:4},
+      {name: "iPad", price: 530, sold:4},
+      {name: "watch", price: 530},
+    ]}
+  ]}
+]
+*/
